bda628e8c471c96e96450ab849057cb75479a9ac
david <david.mazvovsky@gmail.com>
2014-03-09 19:30:25 +0200

    fix

--- formstack-pipedrive-mc.rb
+++ formstack-pipedrive-mc.rb
@@ -44,7 +44,7 @@ class MissionCritical < Sinatra::Base
         value: 0,
         :'081669c9b2a9f8b3d3f8a9ec5a6a4d187ef0c635' => params['Listing URL'],
         :'03dfc0cf0d5dd31c54558f7f3435c3f3eeae3067' => params['Notes/Comments'],
-        stage_id: 38,
+        stage_id: 6,
     }
 
     deal_1 = Pipedrive::Deal.create deal_hash
@@ -122,11 +122,10 @@ class MissionCritical < Sinatra::Base
         person_id: person.id,
         user_id: owner_id,
         value: params['Price'].to_i,
-        stage_id: 6,
+        stage_id: 38,
         status: 'won',
         :'081669c9b2a9f8b3d3f8a9ec5a6a4d187ef0c635' => params['Listing URL'],
         :'03dfc0cf0d5dd31c54558f7f3435c3f3eeae3067' => params['Notes/Comments'],
-        :'3d1b58cafdc6a8b9e9d12ba89cd15dc68aa5cfa8' => params['']
     }
 
     deal_2 = Pipedrive::Deal.create deal_hash
