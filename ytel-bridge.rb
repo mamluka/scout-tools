88af9543a51d4debf38d99aab9e206050400f392
david <david.mazvovsky@gmail.com>
2014-03-12 12:59:35 +0200

    fix

--- ytel-bridge.rb
+++ ytel-bridge.rb
@@ -5,7 +5,7 @@ require_relative 'settings'
 
 #https://docs.google.com/spreadsheet/formResponse?hl=en_US&formkey=fLEDZdERzejNIelEtSkNTMl90ZWdMN1pma0E&first=--A--first_name--B--
 # &last=--A--last_name--B--&phone=--A--phone_number--B--&email=--A--email—B—&organization=--A--address1--B--
-# &address=--A--address2—B—&city=--A--city--B--&state=--A--state—B—&zip=--A--postal_code--B--
+# &address=--A--address2—B—&city=--A--city--B--&state=--A--state--B--&zip=--A--postal_code--B--
 # &alt_phone=--A--alt_phone--B--&agent=--A--fullname—B—&time=—A—SQLdate—B—&notes=--A--comments--B--
 
 class YTel < Sinatra::Base
@@ -26,9 +26,10 @@ class YTel < Sinatra::Base
     ws[new_row_num, 6] = "#{params['address']} #{params['address2']}"
     ws[new_row_num, 7] = params['city']
     ws[new_row_num, 8] = params['state']
-    ws[new_row_num, 9] = params['phone']
-    ws[new_row_num, 10] = params['email']
-    ws[new_row_num, 11] = params['notes']
+    ws[new_row_num, 9] = params['zip']
+    ws[new_row_num, 10] = params['phone']
+    ws[new_row_num, 11] = params['email']
+    ws[new_row_num, 12] = params['notes']
 
     ws.save
 
